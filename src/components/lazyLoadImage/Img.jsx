31dba5dbd3b57f13d7ada43fd9f8384230101b30
tarun6738 <tarunnemali2004@gmail.com>
2024-04-11 11:08:28 +0530

    movix completed

---
+++ src/components/lazyLoadImage/Img.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
+const Img = ({ src, className }) => {
+    return (
+        <LazyLoadImage
+            className={className || ""}
+            alt=""
+            effect="blur"
+            src={src}
+        />
+    );
+};
+
+export default Img;
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {useSelector} from "react-redux"
+import "./style.scss";
+import useFetch from "../../../hooks/useFetch";
+
+import Img from "../../../components/lazyLoadImage/Img";
+
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
+const TMDB_API_KEY = import.meta.env.VITE_APP_TMDB_API_KEY;
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+
+  const navigate = useNavigate();
+    const {url} = useSelector((state) => state.home)
+  const {data, loading} = useFetch(`/movie/upcoming?api_key=${TMDB_API_KEY}`)
+
+  useEffect(()=>{
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path
+    setBackground(bg)
+  },[data])
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  return (
+    <div className="heroBanner">
+        {!loading && <div className="backdrop-img">
+            <Img src={background}/>
+        </div>}
+
+        <div className="opaticy-layer"></div>
+
+        <ContentWrapper>
+      
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or TV show"
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default HeroBanner;
